bb3606fdd5aeff45d0fdd22c9a7c777cabd53d05
healplease <gavaaalex2012@gmail.com>
2025-03-20 14:04:35 +0100

    trying it with radix

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import Components from 'unplugin-vue-components/vite'
+import RadixVueResolver from 'radix-vue/resolver'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [
+        RadixVueResolver({
+          prefix: 'R',
+        })
+      ]
+    })
   ],
   resolve: {
     alias: {
