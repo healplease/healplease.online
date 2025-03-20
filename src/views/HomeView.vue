7f6e4255fc7b7c8ac9ea31db6bbbfdbadc2617d0
healplease <gavaaalex2012@gmail.com>
2025-03-20 14:09:07 +0100

    learning

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    what
   </main>
 </template>
+
+<script lang="ts">
+export default {
+  name: 'HomeView',
+}
+</script>
+
